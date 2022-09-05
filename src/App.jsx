17dece5d5c740bb39d7c5615c7363ec27364c59e
Alvaro R Martin <exagonsoft@gmail.com>
2022-09-05 14:57:44 -0400

    Update App.jsx
    Add Nav Bar Component and set Up

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { useContext, useState } from "react";
 import "./App.css";
+import NavBar from "./components/navBar";
 import Seo from "./components/Seo";
 import { ThemeContext } from "./context/ThemeProvider";
 import { GREETING_DESCRIPTION, IMAGE, KEYWORDS, NAME, URL } from "./data/data";
 
 function App() {
   const { theme, setTheme } = useContext(ThemeContext);
-  const [menuShow, showMenu] = useState(false);
+  const [showMenu, SetShowMenu] = useState(false);
   return(
   <>
     <Seo
@@ -19,6 +20,7 @@ function App() {
       author={NAME}
       theme={theme}
     />
+    <NavBar showMenu={showMenu} SetShowMenu={SetShowMenu}/>
   </>);
 }
 
