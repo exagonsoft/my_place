e9e51235c907ad0c9d27e43ef6d75b78b68049b5
Alvaro R Martin <exagonsoft@gmail.com>
2022-09-05 14:49:25 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
-import { useState } from 'react'
-import './App.css'
+import { useContext, useState } from "react";
+import "./App.css";
+import Seo from "./components/Seo";
+import { ThemeContext } from "./context/ThemeProvider";
+import { GREETING_DESCRIPTION, IMAGE, KEYWORDS, NAME, URL } from "./data/data";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <></>
-  )
+  const { theme, setTheme } = useContext(ThemeContext);
+  const [menuShow, showMenu] = useState(false);
+  return(
+  <>
+    <Seo
+      url={URL}
+      lang="en"
+      metaDescription={GREETING_DESCRIPTION}
+      keywords={KEYWORDS}
+      image={IMAGE}
+      title={NAME}
+      author={NAME}
+      theme={theme}
+    />
+  </>);
 }
 
-export default App
+export default App;
