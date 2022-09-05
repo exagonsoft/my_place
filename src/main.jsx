de721aae27a3fc78e5725fc5b02faa451197bc27
Alvaro R Martin <exagonsoft@gmail.com>
2022-09-05 14:43:45 -0400

    Update main.jsx
    Add Context to the App

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { ThemeContextProvider } from "./context/ThemeProvider";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <ThemeContextProvider>
+      <App />
+    </ThemeContextProvider>
   </React.StrictMode>
-)
+);
