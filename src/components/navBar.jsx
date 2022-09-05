1e5d13a66c3187c06b55d622ba7ca01eb44e2f50
Alvaro R Martin <exagonsoft@gmail.com>
2022-09-05 14:57:10 -0400

    Update navBar.jsx
    Set Up Events

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -1,6 +1,27 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
+import { ThemeContext } from '../context/ThemeProvider';
 
-const NavBar = () => {
+const NavBar = ({props}) => {
+  let listener = null;
+  const [scrollState, setScrollState] = useState(false);
+  const { setTheme } = useContext(ThemeContext);
+  useEffect(() => {
+    listener = document.addEventListener("scroll", () => {
+      var scrolled = document.scrollingElement.scrollTop;
+      if (scrolled >= 120) {
+        if (!scrollState) {
+          setScrollState(true);
+        }
+      } else {
+        if (scrollState) {
+          setScrollState(false);
+        }
+      }
+    });
+    return () => {
+      document.removeEventListener("scroll", listener);
+    };
+  }, [scrollState]);
   return (
     <></>
   )
