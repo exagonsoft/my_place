fbe21e6d5181e8108a986955a2756f6873d9ac63
Alvaro R Martin <exagonsoft@gmail.com>
2022-09-06 03:06:50 -0400

    Create themeList.jsx
    Add a list of Themes

---
+++ src/utils/themeList.jsx
@@ -0,0 +1,8 @@
+export const ThemeList = [
+  { key: 1, title: "Light", name: "light", icon: "🌝" },
+  { key: 2, title: "Dark", name: "dark", icon: "🌚" },
+  { key: 3, title: "Cupcake", name: "cupcake", icon: "🍰" },
+  { key: 4, title: "Bumblebee", name: "bumblebee", icon: "🐝" },
+  { key: 5, title: "Emerald", name: "emerald", icon: "✳️" },
+  { key: 6, title: "Corporate", name: "corporate", icon: "🏢" },
+];
