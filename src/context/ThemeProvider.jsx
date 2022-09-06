24020cbf7f89fc8ceaebf20850cd0b81152ff237
Alvaro R Martin <exagonsoft@gmail.com>
2022-09-06 03:08:31 -0400

    Update ThemeProvider.jsx

--- src/context/ThemeProvider.jsx
+++ src/context/ThemeProvider.jsx
@@ -1,7 +1,14 @@
+import { Children } from "react";
 import { createContext, useState } from "react";
 
-export const ThemeContext = createContext(null)
+export const ThemeContext = createContext(null);
 
-export const ThemeContextProvider = ({children}) => {
-  const [theme, setTheme] = useState('light')
-}
+export const ThemeContextProvider = ({ children }) => {
+  const [theme, setTheme] = useState("dark");
+
+  return (
+    <ThemeContext.Provider value={{ theme, setTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
